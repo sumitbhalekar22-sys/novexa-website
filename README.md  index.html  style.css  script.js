adef882138f66fc656b0bb9414c72319c91adc89
sumitbhalekar22-sys <sumit.bhalekar22@gmail.com>
2026-03-09 18:18:56 +0530

    Create README.md  index.html  style.css  script.js
    git init
    git add .
    git commit -m "novexa website starter"
    git branch -M main
    git remote add origin https://github.com/sumitbhalekar22-sys/novexa-website.git
    git push -u origin main

---
+++ README.md  index.html  style.css  script.js
@@ -0,0 +1,7 @@
+echo "# novexa-website" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/sumitbhalekar22-sys/novexa-website.git
+git push -u origin main
